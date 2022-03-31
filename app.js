becb00f3e87884743ca93e0c0026cc2095fc81e1
dinhtran86 <64117173+dinhtran86@users.noreply.github.com>
2022-04-01 00:13:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,85 +1,96 @@
 const LaplacianKernel = [-1, -1, -1, -1, 8, -1, -1, -1, -1];
+//[-1   -1  -1]
+//[-1    8  -1]
+//[-1   -1  -1]
 
 window.onload = () => {
-    let video = document.querySelector('#myVideo');
-    let canvas = document.querySelector('#myCanvas');
-    let context = canvas.getContext('2d');
+    let myCanvas = document.querySelector('#myCanvas');
+    let context = myCanvas.getContext('2d');
+    
     let back = document.createElement('canvas');
     let backContext = back.getContext('2d');
 
     let canvasWidth, canvasHeight;
 
-    video.addEventListener('play', () => {
-        canvasWidth = video.clientWidth;
-        canvasHeight = video.clientHeight;
-        canvas.width = canvasWidth;
-        canvas.height = canvasHeight;
+    let myVideo = document.querySelector('#myVideo');
+
+    myVideo.addEventListener('play', () => {
+        canvasWidth = myVideo.clientWidth;
+        canvasHeight = myVideo.clientHeight;
+        myCanvas.width = canvasWidth;
+        myCanvas.height = canvasHeight;
         back.width = canvasWidth;
         back.height = canvasHeight;
-        draw(video, context, backContext, canvasWidth, canvasHeight);
+        draw(myVideo, context, backContext, canvasWidth, canvasHeight);
     }, false);
 };
 
-const draw = (video, context, backContext, canvasWidth, canvasHeight) => {
-    if (video.paused || video.ended) {
+
+const RGBtoGrayscale = (imgData) => {
+    let imgMat = imgData.data;
+    
+    for (let i = 0; imgMat.length-i ; i += 4) {
+        let red = imgMat[i];
+        let green = imgMat[i + 1];
+        let blue = imgMat[i + 2];
+        let alpha = (3 * red + 4 * green + blue) >>> 3;
+        imgMat[i] = imgMat[i + 1] = imgMat[i + 2] = alpha;
+    }
+
+    imgData.data = imgMat;
+};
+
+
+// Reference: https://developpaper.com/image-processing-with-convolution-kernel-in-html5-canvas/
+const Convolution = (imgData, outputData, kernel) => {
+    let width = imgData.width,
+        height = imgData.height;
+    let inpData = imgData.data,
+        outpData = outputData.data;
+    let px =4;          // 1 px = 4 [r g b alpha]
+    let kernelSize =3;  // dimension of Laplapcian Kernel
+    for (let j = 1; (height - 1)-j ; ++j) {
+        for (let k = 1; (width - 1)-k ; ++k) {
+            for (let c = 0; kernelSize-c ; ++c) {
+                let i = (j * width + k) * px + c;
+                                            //[-1   -1  -1]
+                                            //[-1    8  -1]    Laplacian Kernel
+                                            //[-1   -1  -1]
+                outpData[i] =   kernel[8] * inpData[i + width * px + px] +  // Lower Right
+                                kernel[7] * inpData[i + width * px] +       // Lower
+                                kernel[6] * inpData[i + width * px - px] +  // Lower Left
+                                kernel[5] * inpData[i + px] +               // Left
+                                kernel[4] * inpData[i] +                    // Center of Mask
+                                kernel[3] * inpData[i - px] +               // Right
+                                kernel[2] * inpData[i - width * px + px] +  // Upper Right
+                                kernel[1] * inpData[i - width * px] +       // Upper
+                                kernel[0] * inpData[i - width * px - px];   // Upper Left
+            }
+            outpData[(j * width + k) * px + kernelSize] = 255;
+        }
+    }
+};
+
+const draw = (myVideo, context, backContext, canvasWidth, canvasHeight) => {
+    if (myVideo.paused || myVideo.ended) {
         return false;
     }
     // Draw the video into the backing canvas
-    backContext.drawImage(video, 0, 0, canvasWidth, canvasHeight);
+    backContext.drawImage(myVideo, 0, 0, canvasWidth, canvasHeight);
 
     // Grab the pixel data from the backing canvas
-    let imgData = backContext.getImageData(0, 0, canvasWidth, canvasHeight);
+    let imgMat = backContext.getImageData(0, 0, canvasWidth, canvasHeight);
 
     // Convert RGB frame to Grayscale frame
-    grayscale(imgData);
+    RGBtoGrayscale(imgMat);
 
-    // Edge detector using Laplacian kernel
-    let outputData = context.createImageData(canvasWidth, canvasHeight);
-    convolve(imgData, outputData, LaplacianKernel);
+    // Edge detector using Laplacian kernel and Convolution
+    let outMat = context.createImageData(canvasWidth, canvasHeight);
+    Convolution(imgMat, outMat, LaplacianKernel);
 
     // Draw the pixels onto the visible canvas
-    context.putImageData(outputData, 0, 0);
+    context.putImageData(outMat, 0, 0);
 
     // Start over
-    setTimeout(draw, 20, video, context, backContext, canvasWidth, canvasHeight);
-
-};
-
-const grayscale = (imgData) => {
-    let data = imgData.data;
-    let dataLength = data.length;
-    for (let i = 0; i < dataLength; i += 4) {
-        let r = data[i];
-        let g = data[i + 1];
-        let b = data[i + 2];
-        let brightness = (3 * r + 4 * g + b) >>> 3;
-        data[i] = data[i + 1] = data[i + 2] = brightness;
-    }
-    imgData.data = data;
-};
-
-const convolve = (imgData, outputData, kernel) => {
-    let w = imgData.width,
-        h = imgData.height;
-    let iD = imgData.data,
-        oD = outputData.data;
-
-    for (let y = 1; y < h - 1; y++) {
-        for (let x = 1; x < w - 1; x++) {
-            for (let c = 0; c < 3; c++) {
-                let i = (y * w + x) * 4 + c;
-                oD[i] =
-                    kernel[0] * iD[i - w * 4 - 4] +
-                    kernel[1] * iD[i - w * 4] +
-                    kernel[2] * iD[i - w * 4 + 4] +
-                    kernel[3] * iD[i - 4] +
-                    kernel[4] * iD[i] +
-                    kernel[5] * iD[i + 4] +
-                    kernel[6] * iD[i + w * 4 - 4] +
-                    kernel[7] * iD[i + w * 4] +
-                    kernel[8] * iD[i + w * 4 + 4];
-            }
-            oD[(y * w + x) * 4 + 3] = 255;
-        }
-    }
+    setTimeout(draw, 20, myVideo, context, backContext, canvasWidth, canvasHeight);
 };
